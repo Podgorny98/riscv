beb98d1f18a9af6e9860ad391cd1918abfd77302
Ilya Podgorny <ilya.podgorny@intel.com>
2019-12-19 16:48:37 +0300

    comment out elf loader

--- simulator.cpp
+++ simulator.cpp
@@ -1,6 +1,6 @@
 #include <functional>
-#include <elf.h>
-#include <gelf.h>
+//#include <elf.h>
+//#include <gelf.h>
 #include "../memory.h"
 #include <err.h>
 #include <fcntl.h>
@@ -16,67 +16,67 @@
 
 void Simulator::load(const char* name)
 {
-    int fd = 0;
-    Elf *e;
-    size_t n;
-    GElf_Phdr phdr;
-    GElf_Shdr shdr;
-    FILE* elf = fopen(name, "rb");
-
-    if (elf_version(EV_CURRENT) == EV_NONE)
-        errx(EX_SOFTWARE, " ELF library initialization failed: %s ", elf_errmsg(-1));
-
-    if ((fd = open(name, O_RDONLY, 0)) < 0)
-        err(EX_NOINPUT, " open \"%s \" failed ", name);
-
-    if ((e = elf_begin(fd, ELF_C_READ, NULL)) == NULL)
-        errx(EX_SOFTWARE, " elf_begin () failed: %s . ", elf_errmsg(-1));
-
-    if (elf_kind(e) != ELF_K_ELF)
-        errx(EX_DATAERR, " \"%s \" is not an ELF object . ", "tmp");
-
-    if (elf_getphdrnum(e, &n) != 0)
-        errx(EX_DATAERR, " elf_getphdrnum() failed: %s. ", elf_errmsg(-1));
-
-    for (int k = 0; k < n; k++)
-    {
-        if (gelf_getphdr(e, k, &phdr) != &phdr)
-            errx(EX_SOFTWARE, " getphdr() failed: %s . ",
-                 elf_errmsg(-1));
-        
-        if (phdr.p_type == PT_LOAD)
-        {
-            fseek(elf, phdr.p_offset, SEEK_SET);
-            uint8_t* buf = (uint8_t*)calloc(phdr.p_memsz, sizeof(uint8_t));
-            fread(buf, sizeof(uint8_t), phdr.p_filesz, elf);
-            riscv.memory.write(phdr.p_vaddr, buf, phdr.p_memsz);
-            free(buf);
-        }
-
-    }
-
-    Elf_Scn* scn = NULL;
-
-    while ((scn = elf_nextscn(e, scn)) != NULL) {
-        gelf_getshdr(scn, &shdr);
-        if (shdr.sh_type == SHT_SYMTAB) {
-            break;
-        }
-    }
-
-    uint32_t startPc = 0;
-    int count = 0;
-    Elf_Data* data = elf_getdata(scn, NULL);
-    count = shdr.sh_size / shdr.sh_entsize;
-    for (int ii = 0; ii < count; ++ii)
-    {
-        GElf_Sym sym;
-        gelf_getsym(data, ii, &sym);
-        if(!strcmp(elf_strptr(e, shdr.sh_link, sym.st_name), "main"))
-            startPc = sym.st_value;
-    }
-
-    riscv.hart.pc = startPc;
+//    int fd = 0;
+//    Elf *e;
+//    size_t n;
+//    GElf_Phdr phdr;
+//    GElf_Shdr shdr;
+//    FILE* elf = fopen(name, "rb");
+//
+//    if (elf_version(EV_CURRENT) == EV_NONE)
+//        errx(EX_SOFTWARE, " ELF library initialization failed: %s ", elf_errmsg(-1));
+//
+//    if ((fd = open(name, O_RDONLY, 0)) < 0)
+//        err(EX_NOINPUT, " open \"%s \" failed ", name);
+//
+//    if ((e = elf_begin(fd, ELF_C_READ, NULL)) == NULL)
+//        errx(EX_SOFTWARE, " elf_begin () failed: %s . ", elf_errmsg(-1));
+//
+//    if (elf_kind(e) != ELF_K_ELF)
+//        errx(EX_DATAERR, " \"%s \" is not an ELF object . ", "tmp");
+//
+//    if (elf_getphdrnum(e, &n) != 0)
+//        errx(EX_DATAERR, " elf_getphdrnum() failed: %s. ", elf_errmsg(-1));
+//
+//    for (int k = 0; k < n; k++)
+//    {
+//        if (gelf_getphdr(e, k, &phdr) != &phdr)
+//            errx(EX_SOFTWARE, " getphdr() failed: %s . ",
+//                 elf_errmsg(-1));
+//
+//        if (phdr.p_type == PT_LOAD)
+//        {
+//            fseek(elf, phdr.p_offset, SEEK_SET);
+//            uint8_t* buf = (uint8_t*)calloc(phdr.p_memsz, sizeof(uint8_t));
+//            fread(buf, sizeof(uint8_t), phdr.p_filesz, elf);
+//            riscv.memory.write(phdr.p_vaddr, buf, phdr.p_memsz);
+//            free(buf);
+//        }
+//
+//    }
+//
+//    Elf_Scn* scn = NULL;
+//
+//    while ((scn = elf_nextscn(e, scn)) != NULL) {
+//        gelf_getshdr(scn, &shdr);
+//        if (shdr.sh_type == SHT_SYMTAB) {
+//            break;
+//        }
+//    }
+//
+//    uint32_t startPc = 0;
+//    int count = 0;
+//    Elf_Data* data = elf_getdata(scn, NULL);
+//    count = shdr.sh_size / shdr.sh_entsize;
+//    for (int ii = 0; ii < count; ++ii)
+//    {
+//        GElf_Sym sym;
+//        gelf_getsym(data, ii, &sym);
+//        if(!strcmp(elf_strptr(e, shdr.sh_link, sym.st_name), "main"))
+//            startPc = sym.st_value;
+//    }
+//
+//    riscv.hart.pc = startPc;
 }
 
 uint32_t Simulator::fetch(address_t address)
